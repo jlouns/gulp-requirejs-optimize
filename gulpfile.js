8af5f3ddbc3059ca51e32fefb7f2753863f824eb
Jonathan Lounsbury <jlouns@gmail.com>
2014-10-29 00:00:42 -0400

    Adding coveralls support

--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,17 @@
 
 var gulp = require('gulp'),
 	jshint = require('gulp-jshint'),
-	mocha = require('gulp-mocha');
+	mocha = require('gulp-mocha'),
+	istanbul = require('gulp-istanbul'),
+	coveralls = require('gulp-coveralls');
 
 var paths = {
-	scripts: './*.js',
+	scripts: './index.js',
 	tests: './test/*.js'
 };
 
 gulp.task('lint', function () {
-	return gulp.src(paths.scripts, paths.tests)
+	return gulp.src([paths.scripts, paths.tests, 'gulpfile.js'])
 		.pipe(jshint())
 		.pipe(jshint.reporter(require('jshint-stylish')));
 });
@@ -18,6 +20,22 @@ gulp.task('lint', function () {
 gulp.task('test', function () {
 	return gulp.src(paths.tests)
 		.pipe(mocha());
+});
+
+gulp.task('coverage', function (done) {
+	gulp.src(paths.scripts)
+		.pipe(istanbul())
+		.on('finish', function () {
+			gulp.src(paths.tests)
+				.pipe(mocha())
+				.pipe(istanbul.writeReports())
+				.on('end', done);
+		});
+});
+
+gulp.task('coveralls', ['coverage'], function () {
+	return gulp.src('./coverage/lcov.info')
+		.pipe(coveralls());
 });
 
 gulp.task('watch', function () {
